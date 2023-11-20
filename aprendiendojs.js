a5fdfa6a6021eccedd7fff24cdc619988d4d335f
Yadyvivian <148440973+Yadyvivian@users.noreply.github.com>
2023-11-20 15:23:19 +0100

    add list shopping

--- aprendiendojs.js
+++ aprendiendojs.js
@@ -3,7 +3,18 @@ let nombre = 'Viviana'
 console.log(nombre)
 
 let a = 20;
-let b = 14;
+let b = 15;
 
 console.log (a + b)
 
+var miListaDeCompras =[["cereal", 3], ["leche", 2], ["galletas", 4], ["pan", 3,], ["refresco", 7]];
+//cereal
+console.log(" voy a comprar " + miListaDeCompras [0][1] + " unidades de " + miListaDeCompras [0][0]+ ".");
+//leche
+console.log(" voy a comprar " + miListaDeCompras [1][1] + " unidades de " + miListaDeCompras [1][0]+ ".");
+//galletas
+console.log(" voy a comprar " + miListaDeCompras [2][1] + " unidades de " + miListaDeCompras [2][0]+ ".");
+//pan
+console.log(" voy a comprar " + miListaDeCompras [3][1] + " unidades de " + miListaDeCompras [3][0]+ ".");
+//refresco
+console.log(" voy a comprar " + miListaDeCompras [4][1] + " unidades de " + miListaDeCompras [4][0]+ ".");
